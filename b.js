003f6b07edda2934f3474e503acf83b6d60235bb
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-14 18:56:03 +0700

    a

--- b.js
+++ b.js
@@ -1,25 +1,33 @@
 const request = require('request')
 
-const URL = "https://api.openweathermap.org/data/2.5/find?q=Saigon&units=metric&appid=86183a23377ed034aef7aad102f43d64"
+// const URL = "https://api.openweathermap.org/data/2.5/find?q=Saigon&units=metric&appid=86183a23377ed034aef7aad102f43d64"
 
-request(URL ,{json : true} ,(error , response , body)=> {
-    // console.log(body)
-        // console.log( body.list[0].main.temp);
-        // if(error){
-        //     console.log(error.message)
-        // }else{
-        //     if(body.cod == "200"){
-        //         console.log(body.list[0].main.temp)
-        //     }else{
-        //         console.log("khong co thanh pho")
-        //     }
-    // }
-    if(error) return console.log(error.message)
-    if(body.cod == "200") return console.log(body.list[0].main.temp)
-    console.log("khong co thanh pho hien tai")
-})
+// request(URL ,{json : true} ,(error , response , body)=> {
+//     // console.log(body)
+//         // console.log( body.list[0].main.temp);
+//         // if(error){
+//         //     console.log(error.message)
+//         // }else{
+//         //     if(body.cod == "200"){
+//         //         console.log(body.list[0].main.temp)
+//         //     }else{
+//         //         console.log("khong co thanh pho")
+//         //     }
+//     // }
+//     if(error) return console.log(error.message)
+//     if(body.cod == "200") return console.log(body.list[0].main.temp)
+//     console.log("khong co thanh pho hien tai")
+// })
 
-function getTemp(city){
-
+function getTemp(city , cb){
+    const URL = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=86183a23377ed034aef7aad102f43d64`
+    request(URL ,{json : true} ,(error , response , body)=> {
+        if(error) return cb(error)
+        if(body.cod == "200") return cb(null , body.list[0].main.temp)
+        cb("khong co thanh pho hien tai" )
+    })
 }
-getTemp("Saigon")
+let a
+getTemp("saigon" , (error , temp) => {
+    console.log(error || temp)
+})
