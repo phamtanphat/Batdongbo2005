c11e3e71b152384cd04bf3a532b1506d59b08c65
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-14 19:16:53 +0700

    a

--- c.js
+++ c.js
@@ -1,5 +1,34 @@
-https://pheptinhonline.herokuapp.com/chia/1/0
+const request = require('request')
 
-cong(10 , 5 ,(error , tong) => {
-    console.log(tong)
+function cong(a , b , cb){
+    const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`
+    request(URL ,{json : true} ,(error , response , body)=> {
+        if(error) return cb(error)
+        cb(null ,body.message)
+    })
+}
+function tru(a , b , cb){
+    const URL = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`
+    request(URL ,{json : true} ,(error , response , body)=> {
+        if(error) return cb(error)
+        cb(null ,body.message)
+    })
+}
+function nhan(a , b , cb){
+    const URL = `https://pheptinhonline.herokuapp.com/nhan/${a}/${b}`
+    request(URL ,{json : true} ,(error , response , body)=> {
+        if(error) return cb(error)
+        cb(null ,body.message)
+    })
+}
+function chia(a , b , cb){
+    const URL = `https://pheptinhonline.herokuapp.com/chia/${a}/${b}`
+    request(URL ,{json : true} ,(error , response , body)=> {
+        if(error) return cb(error)
+        cb(null ,body.message)
+    })
+}
+cong(10 , 10 , (error , tong) => {
+    console.log(error || tong)
 })
+
