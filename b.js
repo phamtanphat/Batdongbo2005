63d6c5b5c4b732fee75b3b628888e6b2a051e8fa
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-14 18:58:26 +0700

    a

--- b.js
+++ b.js
@@ -22,12 +22,12 @@ const request = require('request')
 function getTemp(city , cb){
     const URL = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=86183a23377ed034aef7aad102f43d64`
     request(URL ,{json : true} ,(error , response , body)=> {
-        if(error) return cb(error)
+        if(error) return cb(new Error("loi url"))
         if(body.cod == "200") return cb(null , body.list[0].main.temp)
-        cb("khong co thanh pho hien tai" )
+        cb(new Error("Khong co thanh pho") )
     })
 }
 let a
-getTemp("saigon" , (error , temp) => {
-    console.log(error || temp)
+getTemp("a" , (error , temp) => {
+    console.log(error.message || temp)
 })
