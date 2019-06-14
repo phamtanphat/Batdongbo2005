585231d0fd917d8ddf3be3eca5a85c61f8bad0e9
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-14 18:38:36 +0700

    a

--- b.js
+++ b.js
@@ -4,13 +4,19 @@ const URL = "htt://api.openweathermap.org/data/2.5/find?q=bb&units=metric&appid=
 request(URL ,{json : true} ,(error , response , body)=> {
     // console.log(body)
     // console.log( body.list[0].main.temp);
-    if(error){
-        console.log(error.message)
-    }else{
-        if(body.cod == "200"){
-            console.log(body.list[0].main.temp)
-        }else{
-            console.log("khong co thanh pho")
-        }
+    // if(error){
+    //     console.log(error.message)
+    // }else{
+    //     if(body.cod == "200"){
+    //         console.log(body.list[0].main.temp)
+    //     }else{
+    //         console.log("khong co thanh pho")
+    //     }
+    // }
+    if(error) { 
+       
+        return  console.log(error.message)
     }
+    if(body.cod == "200") return console.log(body.list[0].main.temp)
+    console.log("khong co thanh pho hien tai")
 })
