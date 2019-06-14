489985d40c57c29ed1d219ff6d5d40f5719145b2
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-14 18:15:01 +0700

    a

--- b.js
+++ b.js
@@ -1,4 +1,6 @@
 const request = require('request')
 
 const URL = "https://api.openweathermap.org/data/2.5/find?q=hanoi&units=metric&appid=86183a23377ed034aef7aad102f43d64"
-request()
+request(URL ,{json : true} ,(error , response , body)=> {
+    console.log( body.list[0].main.temp);
+})
