2984888c9eb8a3a740d2c9f0f77c211e06629201
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-17 17:53:49 +0700

    a

--- c.js
+++ c.js
@@ -1,33 +1,39 @@
 const request = require('request')
 
-function cong(a, b, cb) {
-    const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`
-    request(URL, { json: true }, (error, response, body) => {
-        if (error) return cb(error)
-        cb(null, body.message)
+function cong(a, b) {
+    return new Promise((resolve , reject) => {
+        const URL = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`
+        request(URL, { json: true }, (error, response, body) => {
+            if (error) return reject(error)
+            resolve(body.message)
+        })
     })
 }
-function tru(a, b, cb) {
-    const URL = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`
-    request(URL, { json: true }, (error, response, body) => {
-        if (error) return cb(error)
-        cb(null, body.message)
-    })
-}
-function nhan(a, b, cb) {
-    const URL = `https://pheptinhonline.herokuapp.com/nhan/${a}/${b}`
-    request(URL, { json: true }, (error, response, body) => {
-        if (error) return cb(error)
-        cb(null, body.message)
-    })
-}
-function chia(a, b, cb) {
-    const URL = `https://pheptinhonline.herokuapp.com/chia/${a}/${b}`
-    request(URL, { json: true }, (error, response, body) => {
-        if (error) return cb(error)
-        cb(null, body.message)
-    })
-}
+cong(10 , 'x')
+.then(response => console.log(response))
+.catch(error => console.log(error.message))
+
+// function tru(a, b, cb) {
+//     const URL = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`
+//     request(URL, { json: true }, (error, response, body) => {
+//         if (error) return cb(error)
+//         cb(null, body.message)
+//     })
+// }
+// function nhan(a, b, cb) {
+//     const URL = `https://pheptinhonline.herokuapp.com/nhan/${a}/${b}`
+//     request(URL, { json: true }, (error, response, body) => {
+//         if (error) return cb(error)
+//         cb(null, body.message)
+//     })
+// }
+// function chia(a, b, cb) {
+//     const URL = `https://pheptinhonline.herokuapp.com/chia/${a}/${b}`
+//     request(URL, { json: true }, (error, response, body) => {
+//         if (error) return cb(error)
+//         cb(null, body.message)
+//     })
+// }
 // cong(10 , 'x' , (error , tong) => {
 //     if(error) return console.log(error)
 //     nhan(tong , 2 , (error , tich) => {
@@ -36,19 +42,19 @@ function chia(a, b, cb) {
 //     })
 // })
 //1/2 (a+b) x h
-function dientichhinhthang(a, b, h, cb) {
-    cong(a,b , (error, tong) => {
-        if (error) return cb(error)
-        chia(tong, 2, (error, thuong) => {
-            if (error) return cb(error)
-            nhan(thuong , h , (error,tich) => {
-                if(error) return cb(error)
-                cb(null , tich)
-            })
-        })
-    })
-}
-dientichhinhthang(10, 'x', 8, (error, ketqua) => {
-    console.log(error || ketqua)
-})
+// function dientichhinhthang(a, b, h, cb) {
+//     cong(a,b , (error, tong) => {
+//         if (error) return cb(error)
+//         chia(tong, 2, (error, thuong) => {
+//             if (error) return cb(error)
+//             nhan(thuong , h , (error,tich) => {
+//                 if(error) return cb(error)
+//                 cb(null , tich)
+//             })
+//         })
+//     })
+   
+// }
+
+
 
